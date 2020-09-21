c0d566f5bfef56df50ccbda46b3a61efdbb3723d
Harrison <stlebea@foxmail.com>
2020-09-21 21:32:19 +0800

    Delete a.sh

--- a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-status_log=$(git status -sb)
-if [ "$status_log" == "## master...origin/master" ];then
-  echo "nothing to commit, working tree clean"
-else
-  git add .&&git commit -m "update by github actions"&&git push origin master
-fi
